b1105587496f170d91ade399ee2763b7a95b1011
WenerRodrigo <wener19.wr@gmail.com>
2023-04-04 21:25:27 -0300

    finalizado a pagina de favoritos

--- src/pages/Favoritos/index.js
+++ src/pages/Favoritos/index.js
@@ -1,20 +1,24 @@
-import Banner from 'components/Banner';
-import styles from './Favoritos.module.css';
-import Card from 'components/Card';
-import Titulo from 'components/Titulo';
+import Banner from "components/Banner";
+import styles from "./Favoritos.module.css";
+import Card from "components/Card";
+import Titulo from "components/Titulo";
+import { useFavoritoContext } from "contextos/Favoritos";
 
 function Favoritos() {
-    return (
-        <>
-            <Banner img='favoritos.png' />
-            <Titulo>
-                <h1>Meus Favoritos</h1>
-            </Titulo>
-            <section className={styles.container}>
-                <Card id='1' titulo='Gato bonifacio' />
-            </section>
-        </>
-    )
+  const { favorito } = useFavoritoContext();
+  return (
+    <>
+      <Banner img="favoritos.png" />
+      <Titulo>
+        <h1>Meus Favoritos</h1>
+      </Titulo>
+      <section className={styles.container}>
+        {favorito.map((fav) => {
+          return <Card {...fav} key={fav.id} />;
+        })}
+      </section>
+    </>
+  );
 }
 
 export default Favoritos;
