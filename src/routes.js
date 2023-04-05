7e4925825df105d609310cd9cf147d0eaf2f4d33
WenerRodrigo <wener19.wr@gmail.com>
2023-04-05 19:37:49 -0300

    criando rotas aninhadas

--- src/routes.js
+++ src/routes.js
@@ -1,28 +1,21 @@
 import Favoritos from "pages/Favoritos";
 import Inicio from "./pages/Inicio";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Cabecalho from "components/Cabecalho";
-import Rodape from "components/Rodape";
-import Container from "components/Container";
-import FavoritosProvider from "contextos/Favoritos";
 import Player from "pages/Player";
 import NaoEncontrado from "pages/NaoEncontrado";
+import PaginaBase from "pages/PaginaBase";
 
 function AppRoutes() {
   return (
     <BrowserRouter>
-      <Cabecalho />
-      <Container>
-        <FavoritosProvider>
-          <Routes>
-            <Route path="/" element={<Inicio />}></Route>
-            <Route path="/Favoritos" element={<Favoritos />}></Route>
-            <Route path="/:id" element={<Player />}></Route>
-            <Route path="*" element={<NaoEncontrado />}></Route>
-          </Routes>
-        </FavoritosProvider>
-      </Container>
-      <Rodape />
+      <Routes>
+        <Route path="/" element={<PaginaBase />}>
+          <Route index element={<Inicio />}></Route>
+          <Route path="Favoritos" element={<Favoritos />}></Route>
+          <Route path=":id" element={<Player />}></Route>
+          <Route path="*" element={<NaoEncontrado />}></Route>
+        </Route>
+      </Routes>
     </BrowserRouter>
   );
 }
